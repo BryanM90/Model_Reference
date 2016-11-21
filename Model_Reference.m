bf7176647b7853fe1e6f13d6d775a4e4901af6a5
BryanM90 <bryanpm@umich.edu>
2016-11-21 18:22:35 -0500

    Simulation

--- Model_Reference.m
+++ Model_Reference.m
@@ -1,26 +1,35 @@
-%% Project file 1
-%% Bryan Test branch
+% Reference Model for MRAC
+% NA 583
+% University of Michigan - Fall 2016
 
-Ms = 250;
-Mus = 30;
-K2 = 150000;
-K1 = 15000;
-Bm = 1000;
+% Model parameters
+Ms  = 355;      % [kg]
+Mus = 30;       % [kg]
+K1  = 1500;    % [N/m]
+K2  = 15000;   % [N/m]
+Bm  = 3000;     % [N/m/s]
 
+% State Space representation
 A = [    0      1      0        0
       -K2/Mus   0    K1/Mus     0
          0     -1      0        1
          0      0   -K1/Ms   -Bm/Ms ];
 B = [-1 0 0 0]';
-C = [0 0 1 0
-     0 0 0 1];
+C = [0 0 1 0        % y(1) = z_s - z_us (Suspension stroke)
+     0 0 0 1];      % y(2) = \dot(z_s)  (unsprung mass velocity)
 D = [0;0];
 
 sys = ss(A,B,C,D);
-t = 0:0.01:10;
 
-z = lsim(sys,sin(t),t);
-figure;hold on
-plot(t,sin(t))
-plot(t,z(:,1))
-plot(t,z(:,2))
+% Simulation
+t = 0:0.01:10;
+y = lsim(sys,sin(t),t);
+
+% Plot
+figure
+plot(t,sin(t),'r--',t,y(:,1),'k',t,y(:,2),'b-.')
+set(findall(gcf,'type','line'),'linewidth',2)
+set(gca,'fontsize',12); grid on; box on
+leg = legend('Terrain Disturbance','Suspension stroke','Mass velocity');
+set(leg,'interpreter','latex','fontsize',16,'location','southeast');
+xlabel('Time [sec]' ,'interpreter','latex','fontsize',16);
